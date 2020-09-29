991521dffc8c5348baaf833265bf170a1c54875c
Cassie <cassielevett@gmail.com>
2020-09-29 16:25:16 -0500

    save streams

--- stream/client/src/components/App.js
+++ stream/client/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Router } from 'react-router-dom'
+import StreamCreate from '.streams/StreamCreate'
+import StreamDelete from '.streams/StreamDelete'
+import StreamEdit from '.streams/StreamEdit'
+import StreamList from '.streams/StreamList'
+import StreamShow from '.streams/StreamShow'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div>
+        <Router path='/' exact component={StreamList}></Router>
+        <Router path='/streams/new' exact component={StreamCreate}></Router>
+        <Router path='/streams/edit' exact component={StreamEdit}></Router>
+        <Router path='/streams/delete' exact component={StreamDelete}></Router>
+        <Router path='/streams/show' exact component={StreamShow}></Router>
+      </div>
+    </BrowserRouter>
   )
 }
 
