92dae942fdc019b59cfdbe2ef328d152a99321dd
Cassie <cassielevett@gmail.com>
2020-12-01 14:33:13 -0600

    Section 12, Lesson 183

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,8 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 
 const items = [
   {
@@ -19,9 +20,29 @@ const items = [
 ]
 
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: "The Color Green",
+    value: 'green'
+  },
+  {
+    label: "The Color Blue",
+    value: "blue"
+  }
+]
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0])
   return (
     <div className="App">
+      <Dropdown selected={selected} 
+      onSelectedChange={setSelected} 
+      options={options} 
+      />
       <Accordion items={items} />
       <Search />
     </div>
