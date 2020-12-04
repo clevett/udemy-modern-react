f0dcdd6c80592eb40b695e409fa2da2fe51468d5
Cassie <cassielevett@gmail.com>
2020-12-04 14:23:42 -0600

    lesson 346

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -6,11 +6,23 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams()
 	}
+
+	renderAdmin(stream) {
+		if(stream.userId = this.props.currentUserId) {
+			return (
+				<div className='right floated content'>
+					<button className='ui button primary'>Edit</button>
+					<button className='ui button negative'>Delete</button>
+				</div>
+			)
+		}
+	}
 	
 	renderList() {
 		return this.props.streams.map(stream => {
 			return (
 				<div className='item' key={stream.id}>
+					{this.renderAdmin(stream)}
 					<i className='large middle aligned icon camera' />
 					<div className='content'>
 						{stream.title}
@@ -31,6 +43,11 @@ class StreamList extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({ streams: Object.values(state.streams) })
+const mapStateToProps = state => {
+	return {
+		streams: Object.values(state.streams),
+		currentUserId: state.auth.userId
+	}
+}
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList)
